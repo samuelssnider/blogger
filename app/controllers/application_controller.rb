fbe4480cf5517d82efab1f469249549675ce5464
samuelssnider <sam_redins@yahoo.com>
2017-08-27 15:45:12 -0600

    explored several options for creating a new article

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def index
-    @articles = Article.all
-  end
-
-  def show
-    @article = Article.find(params[:id])
-  end
 
 end
